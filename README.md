45eb6cbbe4127ccbd6a3f4850f0599682de40612
im-shadowpool <shadowpoolvs@gmail.com>
2025-11-07 19:53:21 +0530

    added project

--- README.md
+++
@@ -1,2 +0,0 @@
-# House-Of-Godavari-Web
-Website - Frontend for Sweets Website.
---
+++ house-of-godavari-web/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>This is Heading One 1 - Testing</h1>
+      <h2>This is Heading One 1 - Testing</h2>
+      <h3>This is Heading One 1 - Testing</h3>
+      <h4>This is Heading One 1 - Testing</h4>
+      <h5>This is Heading One 1 - Testing</h5>
+      <p>This is a paragraph</p>
+      <b>This is Bold</b>
+      </main>
+  );
+}
